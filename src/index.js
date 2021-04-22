5ad78a42afc26731e6191e565fbe6b340f0a8a35
Don't want to use my name <rwwmr098@gmail.com>
2021-04-22 21:12:24 +0400

    deleting some files and update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { sagaWatcher } from './sagaWatcher'
 import { rootReducer } from './redux/rootReducer'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const saga = createSagaMiddleware()
 const store = createStore(rootReducer, compose(applyMiddleware(thunk, saga)))
@@ -23,7 +22,3 @@ const app = (
 
 ReactDOM.render(app,document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
